c23eb92a89bfa282e476e5ff579d677d3e3a3294
mahamme-siyabuddeen <mohd.sihab615@gmail.com>
2024-09-20 06:48:09 +0530

    type changed

--- src/utility/auth2.ts
+++ src/utility/auth2.ts
@@ -2,7 +2,7 @@ import type {userType1} from '../content/types'
 interface prop {
     value: string
 }
-export async function isAuthenticated2(value: string): Promise<userType|false> {
+export async function isAuthenticated2(value: string): Promise<userType1|false> {
     const response = await fetch(`${import.meta.env.PUBLIC_BACKEND_URL}/api/users/me?populate[carts][populate]=Image`, {
         // populate[carts][populate][products][populate]=Image
         headers: {
